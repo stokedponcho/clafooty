30bdaa1f71e4f3d1b2050d71b40c19dfe4a6ef0b
MournfulFrog <mournfulfrog@snopyta.org>
2022-08-27 19:09:32 +0100

    feat(football-data): accept negative points in standings

--- football_data/src/dtos.rs
+++ football_data/src/dtos.rs
@@ -112,7 +112,7 @@ pub struct Position {
     pub won: u8,
     pub draw: u8,
     pub lost: u8,
-    pub points: u8,
+    pub points: i8,
     pub goals_for: u8,
     pub goals_against: u8,
     pub goal_difference: i8,
